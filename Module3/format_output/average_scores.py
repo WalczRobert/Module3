ab52f6db3758a7a4625e7b8a9388a9a798a319d5
rewalczak <rewalczak@gmail.com>
2020-01-30 09:14:49 -0600

    Add files via upload

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -1,2 +1,22 @@
-def average():
-    pass
+from statistics import mean
+
+
+def information():
+    input1,input2,input3 = input("what are your 3 scores (comma separated): ").split(',')
+    list = [input1, input2, input3]
+    list = [int(i) for i in list]
+    fname = input("What is your first name: ")
+    lname = input("What is your last name: ")
+    age = input("How old are you: ")
+    average(list, lname, fname, age)
+    return list, lname, fname, age
+
+#list, lname,fname,age = information()
+
+def average(list, lname, fname, age):
+    average_score = mean(list)
+    print(f'{lname}, {fname} age {age} average grade {average_score}')
+    return average_score
+
+if __name__ == '__main__':
+    information()
--- Module3/test/average_scores.py
+++ Module3/test/average_scores.py
@@ -1,5 +1,33 @@
-from format_ouput import average_scores
+#from format_output import average()
+from format_output import average_scores
 
-def test_average(self):
-    with mock.patch('bultins.input', side_effects=[85.90.95]):
-        assert topic2.average() == 90
+import unittest
+
+
+class FunctionTestCase(unittest.TestCase):
+    def test_average(self):
+        list = [85, 90, 95]
+        fname = input("What is your first name: ")
+        lname = input("What is your last name: ")
+        age = input("How old are you: ")
+        print(1)
+        self.assertEqual(list, average_scores.average(list))
+        Print(2)
+        assert average_score.average() == 90
+        print(3)
+
+
+
+
+'''class FunctionTestCase(unittest.TestCase):
+    def test_something(self):
+        list = [88, 90, 95]
+        fname = "Robert"
+        lname = "Walczak"
+        age = 45
+       
+        self.assertEqual(list, average_scores.average(list))
+'''
+
+if __name__ == '__main__':
+    unittest.main()
