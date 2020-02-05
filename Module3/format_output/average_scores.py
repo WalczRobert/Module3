3479b791539e8ea80e76cb193d7c14f3deb005ee
rewalczak <rewalczak@gmail.com>
2020-02-04 19:27:58 -0600

    Add files via upload

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -5,16 +5,16 @@ def information():
     input1,input2,input3 = input("what are your 3 scores (comma separated): ").split(',')
     list = [input1, input2, input3]
     list = [int(i) for i in list]
-    fname = input("What is your first name: ")
-    lname = input("What is your last name: ")
+    fname = input("What is your first name: ").capitalize()
+    lname = input("What is your last name: ").capitalize()
     age = input("How old are you: ")
     average(list, lname, fname, age)
-    return list, lname, fname, age
+    #return list, lname, fname, age
 
 #list, lname,fname,age = information()
 
 def average(list, lname, fname, age):
-    average_score = mean(list)
+    average_score = round(mean(list),2)
     print(f'{lname}, {fname} age {age} average grade {average_score}')
     return average_score
 
